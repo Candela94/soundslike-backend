f42b852c3822519cca9105fd0e084bef6528791a
Candela94 <candelafsg@alumnos.cei.es>
2025-06-09 17:16:53 +0200

    controllers

--- controllers/uploads.controllers.js
+++ controllers/uploads.controllers.js
@@ -2,7 +2,7 @@
 
 import { Cancion } from "../db/models/cancion.model.js";
 
-import { createPlaylistByGenero } from "./playlists.controllers";
+import { createPlaylistByGenero } from "./playlists.controllers.js";
 import { BACKEND_URL } from "../config/config";
 
 
